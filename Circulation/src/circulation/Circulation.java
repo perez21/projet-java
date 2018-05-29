bbbd19cb39658ed1dfe4406e5fd2e44bac0bbf73
thewind <thewind@windows>
2018-05-29 23:37:42 +0100

    correction

--- Circulation/src/circulation/Circulation.java
+++ Circulation/src/circulation/Circulation.java
@@ -18,36 +18,32 @@ public class Circulation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-    
-        
-//Creation d'un feu
-        Feu tabfeu[] = {new Feu("Rouge",false),new Feu("Vert",true),new Feu("Jaune",false)};
      
+//Creation d'un feu
+        Feu tabfeu[] = new Feu[3];
+        tabfeu[0]= new Feu("Rouge",false);
+        tabfeu[1]= new Feu("Vert",true);
+        tabfeu[2]= new Feu("Jaune",false);
         
-       
 
 //Creation d'un  tableau de panneaux
         
       Panneau tabpanneau[] = {new Panneau("PSud",tabfeu), new Panneau("PNord",tabfeu),new Panneau("PEst",tabfeu),new Panneau("Pouest",tabfeu)}; 
         
       //Creation d'un tableau de chemin
-      Chemin chemin[] = {new Chemin("cheminSud",tabpanneau[0]), new Chemin("cheminNord",tabpanneau[1]), new Chemin("cheminEst",tabpanneau[2]),new Chemin("cheminOuest",tabpanneau[3])};
-       
-        PassageVehicule();
-    }
-    
-    public static void PassageVehicule(){
-       System.out.println("passage de voiture en cours...");
-        //ici on vas ecrire la methode que nous allons appeler chaque fois que nous voulons afficher le passage de vehicule
-        
-    
-    }
-    
-    public static void PassagePieton(){
-    
-        //ici on vas ecrire la methode que nous allons appeler chaquefois que nous voulons afficher le passage des pietons
-        
+      Chemin tabchemin[] = {new Chemin("cheminSud",tabpanneau[0]), new Chemin("cheminNord",tabpanneau[1]), new Chemin("cheminEst",tabpanneau[2]),new Chemin("cheminOuest",tabpanneau[3])};
+      
+      //Creation du tableau carrefour
+      
+      Carrefour carrefour[] = {new Carrefour("Nord",tabchemin),new Carrefour("Sud",tabchemin),new Carrefour("Est",tabchemin),new Carrefour("Sud",tabchemin)}; 
+      if (carrefour[0].getNom()=="Nord"){
+          if (tabfeu[0].getCouleur()=="Vert"){
+              if (tabfeu[0].isEtat()){
+                  System.out.println("Passage des vehicules du cote Nord et sud");
+              }
+          }
+      } 
+
     }
     
 }
